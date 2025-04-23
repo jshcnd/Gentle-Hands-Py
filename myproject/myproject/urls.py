93195e9e9b6dd9ae3dc64e671026ca7f1adf2585
jinxinyii <pngnbn.elmar@gmail.com>
2025-04-23 13:00:39 +0800

    feat: Add profile picture field to Child model and implement default image handling

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('accounts/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
